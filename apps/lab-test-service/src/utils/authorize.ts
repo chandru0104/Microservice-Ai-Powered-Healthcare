fdefcd4f08af3a69adf7a6bc12acbe0a395c7007
chandru0104 <chandrus0104@gmail.com>
2026-07-03 23:50:44 +0530

    finish lab test api

--- apps/lab-test-service/src/utils/authorize.ts
+++ apps/lab-test-service/src/utils/authorize.ts
@@ -2,7 +2,7 @@ import { Response, NextFunction } from "express"
 
 export const authorize = (dataRole: string) => {
     return async (req: any, res: Response, next: NextFunction) => {
-        const role = await req.user.role
+        const role = await req.user.role 
         if (!role || role.includes(dataRole)) {
             res.status(403).json({
                 success: true,
