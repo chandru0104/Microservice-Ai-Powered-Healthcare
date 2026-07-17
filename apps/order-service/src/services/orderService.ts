7016495a5a329549c1593070ece6b2d6e7cf2a51
chandru0104 <chandrus0104@gmail.com>
2026-07-17 16:33:51 +0530

    start in order service

--- apps/order-service/src/services/orderService.ts
+++ apps/order-service/src/services/orderService.ts
@@ -1,11 +1,9 @@
-import { Order } from "../model/order"
-import { OrderInterface } from "../utils/interfaces"
+// import {Order} from "../model/order"
+// import {OrderInterface} from "../utils/interfaces"
 
-export const addOrderService = async (data: OrderInterface) => {
-    try {
-        const { user, paymetStatus, shippingAddress, items, price } = data
+// export const addOrderService = async(data:OrderInterface)=>{
+//    try{
+//     const {}= data
 
-        const 
-
-    }
-}
+//    }
+// }
