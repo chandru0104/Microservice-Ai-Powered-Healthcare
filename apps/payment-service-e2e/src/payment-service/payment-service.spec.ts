710bdf3d5803bfec731500c309b6b796036dbb83
chandru0104 <chandrus0104@gmail.com>
2026-07-21 21:03:43 +0530

    ai service started

--- apps/payment-service-e2e/src/payment-service/payment-service.spec.ts
+++ apps/payment-service-e2e/src/payment-service/payment-service.spec.ts
@@ -1,4 +1,4 @@
-import require from "supertest"
+import request  from "supertest"
 import dotenv from "dotenv"
 
 
@@ -7,7 +7,7 @@ describe("Payment test",()=>{
     const payload={
       orderId:"1"
     }
-    const res = await require(process.env.PAYMENT_URL as string).post("/api/v1/payment").send(payload)
+    const res = await request(process.env.PAYMENT_URL as string).post("/api/v1/payment").send(payload)
     expect(res.status).toBe(200)
   })
 })
@@ -18,7 +18,7 @@ describe("Verify payment",()=>{
       razorpay_order_id:"32323weqweqw",razorpay_payment_id:"dfsdef312234124",razorpay_signature:"234edwd",receipt:"123ewqeqwed"
     }
 
-    const res = require.post(process.env.PAYMENT_URL as string).post("/api/v1/payment/verify").send(payload)  
+    const res =await request(process.env.PAYMENT_URL as string).post("/api/v1/payment/verify").send(payload)  
     expect(res.status).toBe(200)
   })
 })
