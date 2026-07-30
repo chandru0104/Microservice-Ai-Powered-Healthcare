a9bd0ee3fe97da8356286c2ef2b2f4a52b4154b3
chandru0104 <chandrus0104@gmail.com>
2026-07-31 00:38:04 +0530

    finish appointment service

--- apps/appointment-service-e2e/src/appointment-service/appointment-service.spec.ts
+++ apps/appointment-service-e2e/src/appointment-service/appointment-service.spec.ts
@@ -1,10 +1,26 @@
-import axios from 'axios';
+import request from "supertest";
+import dotenv from "dotenv";
 
-describe('GET /', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/`);
+dotenv.config();
 
-    expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+describe("Appointment API", () => {
+  it("should create an appointment", async () => {
+    const token = "your_jwt_token";
+
+    const result = await request(process.env.APPOINTMENT_SERVICE as string)
+      .post("/api/v1/appointment")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        doctor: "688b7c2d1234567890abcdef",
+        date: "2026-08-01",
+        time: "10:00 AM",
+        day: "Monday",
+        phone: "9876543210",
+        gender: "Male",
+        termsCondition: true,
+        fees: 500,
+      });
+
+    expect(result.statusCode).toBe(201);
   });
 });
