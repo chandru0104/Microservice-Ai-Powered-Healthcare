ff8411a70a237f212f52992e06f5e408f0bb4004
chandru0104 <chandrus0104@gmail.com>
2026-08-14 19:59:39 +0530

    create client folder

--- apps/client-e2e/eslint.config.mjs
+++ apps/client-e2e/eslint.config.mjs
@@ -1,13 +1,12 @@
 import playwright from 'eslint-plugin-playwright';
-import baseConfig from '../../eslint.config.mjs';
+import baseConfig from '../../eslint.base.config.mjs';
 
 export default [
-  ...baseConfig,
   playwright.configs['flat/recommended'],
+  ...baseConfig,
   {
     files: ['**/*.ts', '**/*.js'],
     // Override or add rules here
     rules: {},
   },
 ];
-
--- apps/client/eslint.config.mjs
+++ apps/client/eslint.config.mjs
@@ -1,12 +1,12 @@
 import nextEslintPluginNext from '@next/eslint-plugin-next';
 import nx from '@nx/eslint-plugin';
-import baseConfig from '../../eslint.config.mjs';
+import baseConfig from '../../eslint.base.config.mjs';
 
 export default [
-  ...baseConfig,
+  { plugins: { '@next/next': nextEslintPluginNext } },
   ...nx.configs['flat/react-typescript'],
+  ...baseConfig,
   {
     ignores: ['.next/**/*', '**/out-tsc'],
   },
 ];
-
